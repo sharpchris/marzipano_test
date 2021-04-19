5dfe9ac0b53022624d3b13e45ad49c58022d59a6
Chris Sharp <sharpchris@gmail.com>
2021-04-19 16:53:21 -0400

    initial-commit

---
+++ app-files/data.js
@@ -0,0 +1,94 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20171231_193125_00_102",
+      "name": "IMG_20171231_193125_00_102",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.154409482338691,
+        "pitch": 0.017988500824495546,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5219260033319202,
+          "pitch": 0.4306533719400001,
+          "rotation": 0,
+          "target": "1-img_20171231_194105_00_109"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.16750905214233214,
+          "pitch": 0.45760257634631074,
+          "title": "Butterfly",
+          "text": "Pretty"
+        }
+      ]
+    },
+    {
+      "id": "1-img_20171231_194105_00_109",
+      "name": "IMG_20171231_194105_00_109",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8660033834417433,
+          "pitch": 0.16985949537262712,
+          "rotation": 0,
+          "target": "0-img_20171231_193125_00_102"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
